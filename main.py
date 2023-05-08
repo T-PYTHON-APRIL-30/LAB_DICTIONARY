10af5635e905142f8d8c344fdbb48bcb5bc0c1b5
Yousef M <yousefmalsaadan@gmail.com>
2023-05-08 12:48:04 +0300

    Update the solution

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 #Q1
 phone_book = {
-    "Amal": "0568323222",
-    "Mohammed": "0522222232",
-    "Khadijah": "0532335983",
-    "Abdullah": "0545341144",
-    "Rawan": "0545534556",
-    "Faisal": "0560664566",
-    "Layla": "0567917077"
+    "0568323222": "Amal",
+    "0522222232": "Mohammed",
+    "0532335983": "Khadijah",
+    "0545341144": "Abdullah",
+    "0545534556": "Rawan",
+    "0560664566": "Faisal",
+    "0567917077": "Layla"
 }
 
 input_user = input("Enter the number\n > ")
 if len(input_user) != 10 or input_user.isdigit() == False:
     print("This is invalid number")
-
-
-for key, value in phone_book.items():
-    if input_user == value:
-        print(f"The owner is '{key}'")
-    else:
-        print("Sorry, the number is not found")
+elif input_user in phone_book:
+    print(f"The owner is '{phone_book[input_user]}'")
+else:
+    print("Sorry, the number is not found")
 
 
 #Q2
@@ -28,7 +25,7 @@ def list_number(number: list) -> list:
     for num in number:
         if num == 0:
             number.remove(num)
-            number.insert(-1, num)
+            number.append(num)
     return number
 
 
