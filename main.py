0ccd017f6e49c3998702f62bad6f18f161c44b0b
amalsaab <441102683@student.ksu.edu.sa>
2023-05-08 07:16:25 +0300

    solve the lab

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+'''
 # LAB_DICTIONARY
 
 
 ## Q1:Build a phone book program that receives the phone number (Use Input to let the user provide the number), and returns the name of the owner. 
 You can follow the table below:
 
-| Name    | Number      |
+| Name     | Number     |
 | -------- | ---------- |
 | Amal     | 0568323222 |
 | Mohammed | 0522222232 |
@@ -18,7 +19,36 @@ You can follow the table below:
 - If the number exists, print the owner. Otherwise, print "Sorry, the number is not found".
 - If the number is less or more than 10 numbers, print "This is invalid number".
 - If the number contains letters or symbols, print "This is invalid number".
+'''
+phoneBook = {
+    "0568323222" : "Amal",
+    "0522222232" : "Mohammed",
+    "0532335983" : "Khadijah",
+    "0545341144" : "Abdullah",
+    "0545534556" : "Rawan",
+    "0560664566" : "Faisal",
+    "0567917077" : "Layla"
+}
+number = input("Enter the number you would search: ")
+if len(number) != 10 and not number.isdigit:
+    print("This is invalid number")
+if phoneBook.get(number, -1) == -1:
+    print("Sorry, the number is not found")
+else:
+    print(f"the name of the number ({number}) is {phoneBook[number]}")
+'''
 
 ## Q2:Write a function that receives a list containing the following numbers : 
 - [5, 0, 34, 9, 0, 13, 8]
 - rearranges the list so that the zeros are the end of the list, and finally returns the arranged list.
+
+'''
+def zeroEnd(list : list):
+    for index in range(len(list)):
+        if list[index] == 0:
+            list.pop(index)
+            list.append(0)
+    
+list = [5, 0, 34, 9, 0, 13, 8]
+zeroEnd(list)
+print(list)
