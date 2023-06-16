90e9b7f17382f53534c8db212b9c87afe1902558
hess-01 <hessah-14@hotmail.com>
2023-06-17 00:55:29 +0300

    lab_dictionary0

--- lab_dictionary.py
+++ lab_dictionary.py
@@ -13,11 +13,11 @@ phone_book = {
 number = input("Enter Phone Number:")
 for name, num in phone_book.items():
     if num == number:
-        print "Name Of Owner", name)
-    if num <= 10
-       print "his is invalid number"
-     break
-      else:
+        print ("Name Of Owner", name)
+    if num <= 10:
+       print ("this is invalid number")
+    break
+else:
       print("Sorry, the number is not found")
     
 
@@ -26,13 +26,13 @@ for name, num in phone_book.items():
 #rearranges the list so that the zeros are the end of the list, and finally returns the arranged list
 
 def rearrange_zeros(lst):
-zeros = []
+ zeros = []
 non_zeros = []
-for num in lst:
-   if num == 0"
-      zeros.append(num)
+for num in list:
+   if num == 0:
+      non_zeros.append(num)
    else:
-      non_zeros + zeros
+      non_zeros + non_zeros
 lst = [5, 0, 34, 9, 0, 13, 8]
 print (rearrange_zeros(lst))
 
